9efb78be638c710d390b30b26d456ce930cbe6ab
Chorizard <yaninalperez@gmail.com>
2020-05-07 18:12:03 -0300

    Subo esqueleto de parcial

---
+++ Yanina Perez 2C _ Primer Parcial/Yanina Perez 2C _ Primer Parcial/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+
+namespace Yanina_Perez_2C___Primer_Parcial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
